8bc8299b304e9f7182d92053135270b3f49f2229
Nikolai Filatov <talai-2005@yandex.ru>
2018-07-03 15:15:30 +0300

    1.3. JavaDoc. Code convention [4087]

---
+++ chapter_001/src/ru/job4j/calculate/package-info.java
@@ -0,0 +1,14 @@
+/*
+* Package for calculate task.
+*
+* @author Nikolai Filatov (mailto:talai-2005@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+
+package ru.job4j.calculate;
+
+
+public class NotClearWhichClassToAdd {
+	
+	}
