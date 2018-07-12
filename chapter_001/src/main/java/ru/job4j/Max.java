4314f966a69e62865231280589020179c2e645e0
Nikolai Filatov <talai-2005@yandex.ru>
2018-07-12 15:11:43 +0300

    4.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/Max.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Max {
+
+    public int max(int first, int second){
+
+        int maxValue = (first > second) ? first : second;
+
+        return maxValue;
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/MaxTest.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Nick Filatov (mailto:talai-2005@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class MaxTest {
+
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+}
