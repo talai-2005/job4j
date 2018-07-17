8f78d2edc31067d27b1ffec67fe388c98437dac6
Nikolai Filatov <talai-2005@yandex.ru>
2018-07-17 21:13:30 +0300

    Revised

--- chapter_001/src/main/java/ru/job4j/Max.java
+++ chapter_001/src/main/java/ru/job4j/Max.java
@@ -1,19 +1,14 @@
 package ru.job4j;
 
 public class Max {
-   public int max(int first, int second, int third) {
-        int temp;
-        temp = this.max(second, third);
-        temp = this.max(first, second);
-        temp = this.max(first, third);
-        return temp;
+    public int max(int first, int second, int third) {
+        return this.max(first, this.max(second, third));
     }
 
     public int max(int first, int second) {
-       return (first > second) ? first : second;
-   }
-
- }
+        return first > second ? first : second;
+    }
+}
 
 
 
