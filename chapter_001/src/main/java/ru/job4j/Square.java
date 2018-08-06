ef56ccc6fe16a5454189c78f83e312cef5aa1dd7
Nikolai Filatov <talai-2005@yandex.ru>
2018-08-06 21:35:33 +0300

    6.2. Перевернуть массив. [#4441]

--- chapter_001/src/main/java/ru/job4j/Square.java
+++ chapter_001/src/main/java/ru/job4j/Square.java
@@ -4,7 +4,7 @@ public class Square {
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int a = 0; a < rst.length; a++) {
-            rst [a] = (a+1)*(a+1);
+            rst [a] = (a + 1) * (a + 1);
         }
         return rst;
     }
